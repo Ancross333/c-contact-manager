2f21221285b36148bbc904c6b10be155eb751872
Patrick Shoffner <p_shoffner@outlook.com>
2025-09-21 14:50:39 -0700

    Add placeholders for contact.c methods

--- contact.c
+++ contact.c
@@ -1,17 +1,17 @@
 #include "contact.h"
 
 void list_contacts(contact_t contacts[], int contactCount) {
-
+    printf("list_contacts() called");
 }
 
 void add_contact(contact_t contacts[], int* contactCount) {
-
+    printf("add_contact() called");
 }
 
 void edit_contact(contact_t contacts[], int contactCount) {
-
+    printf("edit_contact() called");
 }
 
 void remove_contact(contact_t contacts[], int* contactCount) {
-
+    printf("remove_contact() called");
 }
